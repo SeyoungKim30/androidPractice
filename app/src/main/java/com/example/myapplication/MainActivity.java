8e6fbb0ffa632c2b02b9017aaedb886066bd2e19
sengu30 <dearseng@gmail.com>
2023-08-05 04:08:15 +0900

    init commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this,"been clicked",Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
+        startActivity(intent);
+    }
+
+    public void onButton3Clicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:010-6380-9098"));
+        startActivity(intent);
+    }
+}
